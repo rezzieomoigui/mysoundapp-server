9b2e316db1854721da89768ea881cfb816af438e
rezzie omoigui <rezzieomoigui.github.io>
2025-06-20 01:57:30 -0400

    update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 const Playlist = require("./models/Playlist");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(cors());
